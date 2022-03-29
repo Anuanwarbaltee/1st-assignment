0fef6e972d0e28f6f68a79d33486e17f0a350d90
Muhammad Anwar <anwardines786@gmail.com>
2022-03-29 05:58:20 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+let key ={
+    id: 1,
+    idCard: 71301-6173292-3,
+    name: "kaxim",
+    age: 19,
+    phoneno: 03445647474,
+    nationality: "Pakisatni",
+    gender: "male",
+    status: "muslim",
+   
+    province: "Punjab",
+    fatherName: "ali",
+   
+    university: "KU",
+    College: "Punjab College",
+  
+
+
+}
+
+let keys = Object.keys(key);
+console.log(keys)
+
+
+
+
+
+
+
+
+let value ={
+    id: 1,
+    name: "Anwar",
+    age: 19,
+    gender: "male",
+    status: "muslim",
+    nationality: "Pakisatni",
+    province: "Punjab",
+    fatherName: "ali",
+    phoneno: 03445647474,
+    university: "KU",
+    College: "Punjab College",
+    idCard: 71301-6173292-3
+
+
+}
+
+let values = Object.values(value);
+console.log(values)
+
+
+
+
+
+
